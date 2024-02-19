9f06991176c111bf7c3ef40cea558ba0f1c96fdf
Hani <106341060+Hani0101@users.noreply.github.com>
2024-02-19 21:19:45 +0200

    Add files via upload

---
+++ 1- Creating a window/Callbacks.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<cstdio>
+#include "Utilities.h"
+static void glfw_error_callback(int error, const char* description)
+{
+	write_log(description);
+	throw("glfw.error");
+}
+
+static void glfw_window_clone_callback(GLFWwindow* window)
+{
+	write_log("window closed");
+}
+
