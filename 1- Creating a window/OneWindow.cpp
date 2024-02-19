63c9e45ca80c95b493eea8f39a72c1cf17a961c1
Hani <106341060+Hani0101@users.noreply.github.com>
2024-02-19 21:48:32 +0200

    Update OneWindow.cpp

--- 1- Creating a window/OneWindow.cpp
+++ 1- Creating a window/OneWindow.cpp
@@ -12,7 +12,7 @@ int main(int argc, char** argv) {
 	GLFWwindow* window = glfwCreateWindow(1200, 800, extract_version(argv[0]), nullptr, nullptr);// extract_version(argv[0]) used to extract the name of our program, full explanation in the utility header
 	glfwMakeContextCurrent(window);
 	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-	glfwSetWindowCloseCallback(window, glfw_window_clone_callback);
+	glfwSetWindowCloseCallback(window, glfw_window_close_callback);
 
 	while (!glfwWindowShouldClose(window))
 	{
